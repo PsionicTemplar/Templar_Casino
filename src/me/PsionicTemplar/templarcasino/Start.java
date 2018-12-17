87bf6bb8184e5ae0800f654eef5b42058f760b40
Administrator <Administrator@INPA-WS04>
2018-12-17 06:44:22 -0500

    Added more to the main config defaults

--- src/me/PsionicTemplar/templarcasino/Start.java
+++ src/me/PsionicTemplar/templarcasino/Start.java
@@ -99,16 +99,46 @@ public class Start extends JavaPlugin {
 		FileConfiguration config = getPlugin().getConfig();
 		config.addDefault("Command.Allow_Single_Poker", Boolean.valueOf(true));
 		config.addDefault("Command.Allow_Multiple_Poker", Boolean.valueOf(true));
+		config.addDefault("Command.Allow_Black_Jack", Boolean.valueOf(true));
 		config.addDefault("Command.Allow_Roulette", Boolean.valueOf(true));
 		config.addDefault("Command.Allow_Craps", Boolean.valueOf(true));
 		config.addDefault("Command.Allow_Keno", Boolean.valueOf(true));
 		config.addDefault("Command.Allow_Slots", Boolean.valueOf(true));
+		config.addDefault("Command.Allow_Bingo", Boolean.valueOf(true));
 		config.addDefault("Command.Purchase.Allow_Scratch", Boolean.valueOf(true));
 		config.addDefault("Command.Purchase.Allow_Lotto", Boolean.valueOf(true));
-		config.addDefault("Command.Purchase.Scratch_Price", (float) 10.0);
-		config.addDefault("Command.Purchase.Lotto_Price", (float) 5.0);
 		config.addDefault("Command.Purchase.Admin.Allow_Scratch", Boolean.valueOf(true));
 		config.addDefault("Command.Purchase.Admin.Allow_Lotto", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Single_Poker", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Multiple_Poker", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Black_Jack", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Roulette", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Craps", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Keno", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Slots", Boolean.valueOf(true));
+		config.addDefault("Command.Purchase.Admin.Allow_Bingo", Boolean.valueOf(true));
+		config.addDefault("Min_Bet.Scratch_Price", (float) 10.0); //Per scratch off
+		config.addDefault("Min_Bet.Lotto_Price", (float) 5.0); //Per ticket
+		config.addDefault("Min_Bet.Single_Poker", (float) 0.25); //Per round
+		config.addDefault("Min_Bet.Multiple_Poker", (float) 3.0); //For first call
+		config.addDefault("Min_Bet.Black_Jack", (float) 1.0); //Per round
+		config.addDefault("Min_Bet.Roulette", (float) 3.0); //At least this much on the board
+		//TODO Look these next 2 up and make sure it's accurate
+		config.addDefault("Min_Bet.Craps", (float) 1.0); //Per ?
+		config.addDefault("Min_Bet.Keno", (float) 1.0);  //Per ?
+		//TODO Maybe make more than one type of slots?
+		config.addDefault("Min_Bet.Slots", (float) 0.1); //Per Line
+		config.addDefault("Min_Bet.Bingo", (float) 5.0); //Per Card
+		config.addDefault("Block.Allow_Single_Poker", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Multiple_Poker", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Black_Jack", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Roulette", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Craps", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Keno", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Slots", Boolean.valueOf(true));
+		config.addDefault("Block.Allow_Bingo", Boolean.valueOf(true));
+		config.addDefault("Block.Purchase.Allow_Scratch", Boolean.valueOf(true));
+		config.addDefault("Block.Purchase.Allow_Lotto", Boolean.valueOf(true));
 	}
 
 }
