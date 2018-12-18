2f4d065b457953d1dd9eb4b9dfb05e39f63e8db2
Administrator <Administrator@INPA-WS04>
2018-12-18 03:45:43 -0500

    Got rid of bad code. Fixed a bug or 2. Added documentation to CardType.

--- src/me/PsionicTemplar/templarcasino/Util/CardType.java
+++ src/me/PsionicTemplar/templarcasino/Util/CardType.java
@@ -1,21 +1,29 @@
 package me.PsionicTemplar.templarcasino.Util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public enum CardType {
 	SPADE, CLUB, DIAMOND, HEART;
-	
-	private static HashMap<CardType, Integer> loadCardDeck() {
-		HashMap<CardType, Integer> deck = new HashMap<CardType, Integer>();
-		for(CardType ct : CardType.values()) {
-			for(int i = 1; i < 13; i++) {
-				deck.put(ct, i);
+
+	/**
+	 * Creates a new hashmap with all the cards put inside of them.
+	 * 
+	 * @author Nicholas Braniff
+	 * @return Hashmap with all 4 card types as keys and values for those keys as
+	 *         list with 1-13. 1 = Ace, 11 = Jack, 12 = Queen, and 13 = King.
+	 */
+
+	public static HashMap<CardType, List<Integer>> getCardDeck() {
+		HashMap<CardType, List<Integer>> deck = new HashMap<CardType, List<Integer>>();
+		for (CardType ct : CardType.values()) {
+			List<Integer> list = new ArrayList<Integer>();
+			for (int i = 1; i < 14; i++) {
+				list.add(i);
 			}
+			deck.put(ct, list);
 		}
 		return deck;
-	}
-	
-	public static HashMap<CardType, Integer> getCardDeck() {
-		return loadCardDeck();
 	}
 }
